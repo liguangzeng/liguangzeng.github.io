f45e166f4cdc1114fda0d74e9dbe712c678f29fd
誓 <821222024@qq.com>
2021-01-20 15:18:35 +0800

    提交代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: '@vuepress/blog',
     head:[
         // 注入到当前页面的 HTML <head> 中的标签
-        ['link',{rel:"icon",href:"/img/favicon.ico"}]// 增加一个自定义的 favicon(网页标签的图标)
+        ['link',{rel:"icon",href:"/favicon.ico"}]// 增加一个自定义的 favicon(网页标签的图标)
     ],
     serviceWorker: true, // 是否开启 PWA
     base: '/', // 这是部署到github相关的配置
@@ -20,8 +20,8 @@ module.exports = {
         },
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'External', link: 'https://google.com' },
+            { text: 'Tag', link: '/tag/'}
         ]
-    }
+    },
+    evergreen:true
 };
